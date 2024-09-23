f1320cf44a6955d19fadf564ad4d4a93310ba19b
JpCorpse <johnpatrickmirabueno191@gmail.com>
2024-09-23 16:30:15 +0800

    Add files via upload

--- src/components/Back.jsx
+++ src/components/Back.jsx
@@ -11,7 +11,7 @@ const Back = () => {
       <img className="w-full h-screen object-cover " src={Logo} alt="/" />
       <div className="w-full h-screen absolute top-0 left-0 bg-white/30">
         <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center items-center">
-          <h1 className="sm:text-4xl text-3xl font-bold text-gray-800">
+          <h1 className="sm:text-4xl text-3xl font-bold text-gray-800 text-center">
             I'm John Patrick V. Mirabueno
           </h1>
           <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
